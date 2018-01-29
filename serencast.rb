1ecc246cd7d4b4b9b83dfadebe187a9514587f8a
Toshiyuki Masui <masui@pitecan.com>
2018-01-29 16:53:17 +0900

    ...

--- serencast.rb
+++ serencast.rb
@@ -6,7 +6,8 @@ $:.unshift File.expand_path 'lib', File.dirname(__FILE__)
 require 'rubygems'
 
 require 'sinatra'
-require 'sinatra/cross_origin'
+# require 'sinatra/cross_origin'
+require 'cross_origin'
 
 require 'gethistory'
 require 'getsbdata'
